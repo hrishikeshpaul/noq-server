373a3c1ef6604b2963a7727a54dd0a345b585f27
sharavi <sharavi@iu.edu>
2019-10-22 10:15:05 -0400

    testing route

--- index.js
+++ index.js
@@ -9,4 +9,4 @@ app
 	.set('view engine', 'ejs')
 	.get('/', (req, res) => res.render('pages/index'))
 	.listen(PORT, () => console.log(`Listening on ${PORT}`))
-app.use('/', home);
+app.use('/home', home);
