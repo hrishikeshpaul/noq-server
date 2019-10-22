192eb9e43ddc3130d9bda00f311914a0c90a8441
sharavi <sharavi@iu.edu>
2019-10-22 10:13:15 -0400

    testing route

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 const express = require('express')
 const path = require('path')
 const PORT = process.env.PORT || 5000
-
-express()
-  .use(express.static(path.join(__dirname, 'public')))
-  .set('views', path.join(__dirname, 'views'))
-  .set('view engine', 'ejs')
-  .get('/', (req, res) => res.render('pages/index'))
-  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+var app = express();
+var home = require('./routes/test');
+app
+	.use(express.static(path.join(__dirname, 'public')))
+	.set('views', path.join(__dirname, 'views'))
+	.set('view engine', 'ejs')
+	.get('/', (req, res) => res.render('pages/index'))
+	.listen(PORT, () => console.log(`Listening on ${PORT}`))
+app.use('/', home);
---
+++ routes/test.js
@@ -0,0 +1,11 @@
+
+var express = require('express');
+
+var router = express.Router();
+
+
+
+router.get('/', function (req, res, next) {
+	res.send('hiiii')
+})
+module.exports = router;
