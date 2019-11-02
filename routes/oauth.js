c2f03d18f3d83351d36605f83ee13374408a24cb
sharavi <sharavi@iu.edu>
2019-11-02 19:04:56 -0400

    request made variable

--- routes/oauth.js
+++ routes/oauth.js
@@ -1,6 +1,6 @@
 var express = require('express')
 var router = express.Router();
-const request = require('request');
+var request = require('request');
 var User = require('../models/User')
 
 class LinkedInAPI {
