2f5a98ddff7b531cd6df1dd743847f5aeb9bb40f
sharavi <sharavi@iu.edu>
2019-11-04 10:13:10 -0500

    localhost replaced with URL

--- routes/auth.js
+++ routes/auth.js
@@ -134,7 +134,7 @@ router.get('/resend/:id', function (req, res, next) {
 			if (err) { return res.status(500).send({ msg: err.message }); }
 
 			// Send the email
-			var transporter = nodemailer.createTransport({ service: 'Sendgrid', auth: { user: 'hrishikeshpaul', pass: 'Keshpaul1996' } });
+			var transporter = nodemailer.createTransport({ service: 'Sendgrid', auth: { user: 'noqjobporta', pass: 'jobportal12345' } });
 			var mailOptions = { from: 'no-reply@codemoto.io', to: user.email, subject: 'Account Verification Token', text: 'Hello,\n\n' + 'Please verify your account by clicking the link: \nhttp:\/\/' + req.headers.host + '\/api\/auth\/confirmation\/' + token.token + '.\n' };
 			transporter.sendMail(mailOptions, function (err) {
 				if (err) { return res.status(500).send({ msg: err.message }); }
@@ -172,8 +172,8 @@ router.post('/forgot', function (req, res, next) {
 			var smtpTransport = nodemailer.createTransport({
 				service: 'SendGrid',
 				auth: {
-					user: 'hrishikeshpaul',
-					pass: 'Keshpaul1996'
+					user: 'noqjobporta',
+					pass: 'jobportal12345'
 				}
 			});
 			var mailOptions = {
@@ -228,8 +228,8 @@ router.post('/reset/:token', function (req, res) {
 			var smtpTransport = nodemailer.createTransport({
 				service: 'SendGrid',
 				auth: {
-					user: 'hrishikeshpaul',
-					pass: 'Keshpaul1996'
+					user: 'noqjobporta',
+					pass: 'jobportal12345'
 				}
 			});
 			var mailOptions = {
