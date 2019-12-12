6762d2bdc50d42d57c2ed883752f60f71ffaa2ef
hrishikeshpaul <hrishikeshpaul12@gmail.com>
2019-12-11 20:00:30 -0500

    heroku build

--- config/server_config.js
+++ config/server_config.js
@@ -4,7 +4,8 @@ module.exports = {
 		client: 'http://localhost:8080'
 	},
 	dev: {
-		server: "https://ancient-caverns-78426.herokuapp.com",
+		// server: "https://ancient-caverns-78426.herokuapp.com",
+		server: 'https://noq-server1.herokuapp.com',
 		client: 'https://noq-client.herokuapp.com'
 	}
 }
